f98583a4fa7128461f6f00f64869027426bad6ba
Prajwal <prajwal.khambad21@vit.edu>
2023-04-04 21:13:17 +0530

    random images

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from datetime import timedelta, datetime
 import io
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request, session, url_for
 import base64
 from io import BytesIO
 import requests
@@ -9,6 +9,7 @@ import firebase_admin
 from firebase_admin import credentials, auth, storage
 from PIL import Image
 import numpy as np
+import random
 
 app = Flask(__name__)
 
@@ -112,59 +113,39 @@ def my_fun():
 
     return render_template("index2.html", encoded_imgs=encoded_imgs)
 
-# @app.route('/half')
-# def my_fun():
-#     bucket = storage.bucket()
-#     blob = bucket.blob(f'{user.uid}.jpg')
-#     expiration_time = timedelta(minutes=5)
-#     image_url = blob.generate_signed_url(expiration=datetime.utcnow() + expiration_time)
-#     response =requests.get(image_url)
-#     img = Image.open(BytesIO(response.content))
-#     img = np.array(img)
-#     image = cv2.resize(img, (500,500))
-
-#     # Divide the image into four equal parts
-#     (h,w)=image.shape[:2]
-#     (h_step, w_step)=(h//2, w//2)
-#     parts=[image[:h_step, :w_step], 
-#            image[:h_step, w_step:], 
-#            image[h_step:, :w_step], 
-#            image[h_step:, w_step:]]
-
-#     encoded_imgs=[]
-#     for part in parts:
-#         _, data = cv2.imencode(".jpg", part)
-#         encoded_part = base64.b64encode(data).decode('utf-8')
-#         encoded_imgs.append(encoded_part)
-
-#     return render_template("index2.html", encoded_imgs=encoded_imgs)
-
-@app.route('/show_images', methods=['GET', 'POST'])
-def show_images():
+@app.route('/login', methods=['GET', 'POST'])
+def login_page():
     if request.method == 'POST':
         email = request.form['email']
-        
-        # Retrieve images from Firebase storage
+
+        # retrieve images from firestore storage
         bucket = storage.bucket()
         blobs = bucket.list_blobs()
-        images = []
         expiration_time = timedelta(minutes=5)
+        all_images = []
+        some_images = []
         for blob in blobs:
-            # if blob.content_type.startswith(''):
-            # images.append(blob.public_url)
-            images.append(blob.generate_signed_url(expiration=datetime.utcnow() + expiration_time))
+            all_images.append(blob.generate_signed_url(expiration=datetime.utcnow() + expiration_time))
+
+        # get user's specific image by id
+        try:
+            user_ = auth.get_user_by_email(email)
+        except:
+            return "User not found", 404
         
-        # Get the user's specific image by ID
-        # user_id = get_user_id_by_email(email) # replace with your own function to get user id
-        # specific_image = f'images/{user_id}.jpg'
-        # specific_image_url = storage.blob(specific_image).public_url
-        specific_image_url = "https://firebasestorage.googleapis.com/v0/b/advanced-authentication-3ba33.appspot.com/o/GrAybg5GMVZ6hDxT3FuEMei894K2.jpg?alt=media&token=ce960afd-5726-453e-93f7-f091873dfbd1"
-        
-        return render_template('login.html', images=images, specific_image_url=specific_image_url)
+        if user_:
+            blob_2 = bucket.blob(f'{user_.uid}.jpg')
+            some_images.append(blob_2.generate_signed_url(expiration=datetime.utcnow() + expiration_time))
+
+        # Randomly select 6 images from the list of all images
+        images = random.sample(all_images, 5)
+        for ele in range(5):
+            some_images.append(images[ele])
+        random.shuffle(some_images)
+
+        return render_template('login.html', images=some_images)
     
     return render_template('login.html')
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
